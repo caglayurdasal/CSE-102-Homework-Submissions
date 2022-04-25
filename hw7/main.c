f9a41d4e61a6d86d1fba7f4d618b716adc5a81f4
cagla <caglayurdasal@gmail.com>
2022-04-26 01:38:02 +0300

    Get words with fscanf

--- hw7/main.c
+++ hw7/main.c
@@ -7,11 +7,13 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define ROW 15
 #define COLUMN 15
+#define MAX_LEN 10
 
 int main(void)
 {
@@ -19,18 +21,34 @@ int main(void)
     int size_file;     // size of wordlist.txt
     int wordCount = 0; // word count selected for the board
     char board[ROW][COLUMN];
-    char wordList[7];
-    char c;
+    char wordList[7][MAX_LEN];
+    char temp[7][MAX_LEN];
+
     fp = fopen("wordlist.txt", "r+");
 
-    int num_lines = 0; // number of lines in .txt file
-    while (!feof(fp))
+    fseek(fp, 0, SEEK_END); /* Go to end of file to find size */
+    size_file = ftell(fp);  /* Store the current position of file
+                               pointer to get the size of file*/
+
+    srand(time(NULL)); // Seed
+
+    if (fp != NULL)
     {
-        c = fgetc(fp);
-        if (c == '\n')
+        while (wordCount < 7) /* Select random 7 words for the board */
         {
-            num_lines++;
+
+            fseek(fp, -1 * (rand() % size_file), SEEK_CUR); /* Move the position of file pointer back
+                                                               to a random position */
+
+            fscanf(fp, "%s", temp[wordCount]); /* In case pointer is not at the start of a word,
+                                                  scan until end of line and put the scanned word
+                                                  in temp to discard it, then read the next word
+                                                  from the start and put it in the word list  */
+            fscanf(fp, "%s", wordList[wordCount]);
+
+            printf("%d. word: %s\n", wordCount + 1, wordList[wordCount]);
+            fseek(fp, 0, SEEK_END); /* Go to end of file for the next iteration */
+            wordCount++;
         }
     }
-    printf("Lines: %d\n", num_lines);
 }
