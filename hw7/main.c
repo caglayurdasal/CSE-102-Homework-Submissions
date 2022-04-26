640f870179c1aeb22dbda05068294dbbd68e0f24
cagla <caglayurdasal@gmail.com>
2022-04-26 03:09:48 +0300

    Add buildBoard function to main.c

--- hw7/main.c
+++ hw7/main.c
@@ -15,6 +15,44 @@
 #define COLUMN 15
 #define MAX_LEN 10
 
+void buildBoard(char *words[][MAX_LEN])
+{
+    char board[ROW][COLUMN];
+    int dir[8], y_axis, x_axis, len; //(y,x)=(row,column)
+    srand(time(NULL));               // seed
+    for (int i = 0; i < 7; i++)
+    {
+        /* Mark start point of the word at the board */
+        y_axis = rand() % 15;
+        x_axis = rand() % 15;
+        len = strlen(words[i]);
+
+        switch (dir[i])
+        {
+        case 1:
+            /* Place the word from left to right */
+
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        default:
+            break;
+        }
+    }
+}
+
 int main(void)
 {
     FILE *fp;
