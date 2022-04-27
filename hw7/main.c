13edf8a3892ed86ecc8636d69e4fd9ea0969ebe3
caglayurdasal <caglayurdasal@gmail.com>
2022-04-27 23:57:24 +0300

    Get random words with fgets

--- hw7/main.c
+++ hw7/main.c
@@ -14,70 +14,7 @@
 #define ROW 15
 #define COLUMN 15
 #define MAX_LEN 10
-
-void
-buildBoard (char board[ROW][COLUMN], char word[MAX_LEN])
-{
-  /* Put spaces initially inside the board for later check with characters */
-  for (int r = 0; r < ROW; r++)
-    {
-      for (int c = 0; c < ROW; c++)
-        {
-          board[r][c] = " ";
-        }
-    }
-  int dir[8], y_axis, x_axis, len_word; //(y,x)=(row,column)
-  srand (time (NULL));                  // seed
-  for (int i = 0; i < 7; i++)
-    {
-      /* Get the start point of the word on the board */
-      y_axis = rand () % 15; // index 0-14
-      x_axis = rand () % 15; // index 0-14
-      len_word = strlen (word[i]);
-      dir[i] = rand () % 8 + 1; // directions 1-8
-
-      switch (dir[i])
-        {
-        case 1:
-          /* Place the word from left to right */
-          left_to_right (board, word, y_axis, x_axis, len_word);
-          while (left_to_right == -1)
-            {
-            }
-          break;
-        case 2:
-          /* Place the word from right to left */
-          right_to_left (board, word, y_axis, x_axis, len_word);
-          break;
-        case 3:
-          /* Place the word upwards */
-          upwards (board, word, y_axis, x_axis, len_word);
-          break;
-        case 4:
-          /* Place the word downwards */
-          downwards (board, word, y_axis, x_axis, len_word);
-          break;
-        case 5:
-          /* Place the word downward diagonal, left to right */
-          diag_down_left_to_right (board, word, y_axis, x_axis, len_word);
-          break;
-        case 6:
-          /* Place the word downward diagonal, right to left */
-          diag_down_right_to_left (board, word, y_axis, x_axis, len_word);
-          break;
-        case 7:
-          /* Place the word upward diagonal, left to right */
-          diag_up_left_to_right (board, word, y_axis, x_axis, len_word);
-          break;
-        case 8:
-          /* Place the word upward diagonal, right to left */
-          diag_up_right_to_left (board, word, y_axis, x_axis, len_word);
-          break;
-        default:
-          break;
-        }
-    }
-}
+#define NUM_WORDS 7
 
 int
 diag_down_left_to_right (char board[ROW][COLUMN], char word[], int y_axis,
@@ -90,7 +27,7 @@ diag_down_left_to_right (char board[ROW][COLUMN], char word[], int y_axis,
         {
           return -1;
         }
-      else if (board[y_axis][x_axis] != " ")
+      else if (strcmp (&board[y_axis][x_axis], " ") != 0)
         {
           return -1;
         }
@@ -118,7 +55,7 @@ diag_down_right_to_left (char board[ROW][COLUMN], char word[], int y_axis,
         {
           return -1;
         }
-      else if (board[y_axis][x_axis] != " ")
+      else if (strcmp (&board[y_axis][x_axis], " ") != 0)
         {
           return -1;
         }
@@ -147,7 +84,7 @@ diag_up_left_to_right (char board[ROW][COLUMN], char word[], int y_axis,
         {
           return -1;
         }
-      else if (board[y_axis][x_axis] != " ")
+      else if (strcmp (&board[y_axis][x_axis], " ") != 0)
         {
           return -1;
         }
@@ -174,7 +111,7 @@ diag_up_right_to_left (char board[ROW][COLUMN], char word[], int y_axis,
         {
           return -1;
         }
-      else if (board[y_axis][x_axis] != " ")
+      else if (strcmp (&board[y_axis][x_axis], " ") != 0)
         {
           return -1;
         }
@@ -201,7 +138,7 @@ left_to_right (char board[ROW][COLUMN], char word[], int y_axis, int x_axis,
         {
           return -1;
         }
-      else if (board[y_axis][x_axis] != " ")
+      else if (strcmp (&board[y_axis][x_axis], " ") != 0)
         {
           return -1;
         }
@@ -227,7 +164,7 @@ right_to_left (char board[ROW][COLUMN], char word[], int y_axis, int x_axis,
         {
           return -1;
         }
-      else if (board[y_axis][x_axis] != " ")
+      else if (strcmp (&board[y_axis][x_axis], " ") != 0)
         {
           return -1;
         }
@@ -236,7 +173,7 @@ right_to_left (char board[ROW][COLUMN], char word[], int y_axis, int x_axis,
   i = 0;
   while (i < len_word)
     {
-      if (board[y_axis][x_axis] == " ")
+      if (strcmp (&board[y_axis][x_axis], " ") != 0)
         {
           board[y_axis][x_axis] = word[i];
           x_axis--;
@@ -260,7 +197,7 @@ upwards (char board[ROW][COLUMN], char word[], int y_axis, int x_axis,
         {
           return -1;
         }
-      else if (board[y_axis][x_axis] != " ")
+      else if (strcmp (&board[y_axis][x_axis], " ") != 0)
         {
           return -1;
         }
@@ -286,7 +223,7 @@ downwards (char board[ROW][COLUMN], char word[], int y_axis, int x_axis,
         {
           return -1;
         }
-      else if (board[y_axis][x_axis] != " ")
+      else if (strcmp (&board[y_axis][x_axis], " ") != 0)
         {
           return -1;
         }
@@ -303,6 +240,65 @@ downwards (char board[ROW][COLUMN], char word[], int y_axis, int x_axis,
   return 0;
 }
 
+void
+buildBoard (char board[ROW][COLUMN], char words[])
+{
+  /* Put spaces initially inside the board for later check with characters */
+  for (int r = 0; r < ROW; r++)
+    {
+      for (int c = 0; c < ROW; c++)
+        {
+          strcpy (&board[r][c], "-");
+        }
+    }
+  int dir, y_axis, x_axis, len_word; //(y,x)=(row,column)
+  srand (time (NULL));               // seed
+
+  /* Get the start point of the word on the board */
+  y_axis = rand () % 15; // index 0-14
+  x_axis = rand () % 15; // index 0-14
+  len_word = strlen (words);
+  dir = rand () % 8 + 1; // directions 1-8
+  printf ("Dir for the %s: %d\n", words, dir);
+
+  switch (dir)
+    {
+    case 1:
+      /* Place the word from left to right */
+      left_to_right (board, words, y_axis, x_axis, len_word);
+      break;
+    case 2:
+      /* Place the word from right to left */
+      right_to_left (board, words, y_axis, x_axis, len_word);
+      break;
+    case 3:
+      /* Place the word upwards */
+      upwards (board, words, y_axis, x_axis, len_word);
+      break;
+    case 4:
+      /* Place the word downwards */
+      downwards (board, words, y_axis, x_axis, len_word);
+      break;
+    case 5:
+      /* Place the word downward diagonal, left to right */
+      diag_down_left_to_right (board, words, y_axis, x_axis, len_word);
+      break;
+    case 6:
+      /* Place the word downward diagonal, right to left */
+      diag_down_right_to_left (board, words, y_axis, x_axis, len_word);
+      break;
+    case 7:
+      /* Place the word upward diagonal, left to right */
+      diag_up_left_to_right (board, words, y_axis, x_axis, len_word);
+      break;
+    case 8:
+      /* Place the word upward diagonal, right to left */
+      diag_up_right_to_left (board, words, y_axis, x_axis, len_word);
+      break;
+    default:
+      break;
+    }
+}
 int
 main (void)
 {
@@ -310,39 +306,88 @@ main (void)
   int size_file;     // size of wordlist.txt
   int wordCount = 0; // word count selected for the board
   char board[ROW][COLUMN];
-  char wordList[7][MAX_LEN];
+  char wordList[NUM_WORDS][MAX_LEN];
+  char word[MAX_LEN];
   char temp[7][MAX_LEN];
 
   fp = fopen ("wordlist.txt", "r+");
+  /* Go to end of file to find size */
+  // fseek (fp, 0, SEEK_END);
+  /* Store the current position of file pointer to get the size of file */
+  // size_file = ftell (fp);
 
-  fseek (fp, 0, SEEK_END); /* Go to end of file to find size */
-  size_file = ftell (fp);  /* Store the current position of file
-                              pointer to get the size of file */
-
-  if (fp != NULL)
+  /* Count words in file */
+  while (fgets (word, sizeof (word), fp) != NULL)
     {
-      srand (time (NULL));  // seed
-      while (wordCount < 7) /* Select random 7 words for the board */
+      wordCount++;
+    }
+  // printf ("Words: %d\n", wordCount);
+  /* Store random words in array */
+  srand ((unsigned)time (NULL)); // seed
+  for (int i = 0; i < NUM_WORDS; i++)
+    {
+      rewind (fp);
+
+      int sel = rand () % wordCount + 1;
+      for (int j = 0; j < sel; j++)
         {
-          fseek (fp, -1 * (rand () % size_file),
-                 SEEK_CUR); /* Move the position of file pointer back
-                              to a random position */
+          fgets (word, sizeof (word), fp);
+        }
+      strcpy (wordList[i], word);
+      for (int j = 0; j < i; j++)
+        {
+          while (strcmp (wordList[i], wordList[j]) == 0)
+            {
+              printf ("Duplicate word: %s in index: %d\n", wordList[j], j);
+              srand ((unsigned)time (NULL)); // seed
+              for (int i = 0; i < NUM_WORDS; i++)
+                {
+                  rewind (fp);
 
-          fscanf (fp, "%s",
-                  temp[wordCount]); /* In case pointer is not at the start of a
-                                     word, scan until end of line and put the
-                                     scanned word in temp to discard it, then
-                                     read the next word from the start and
-                                     put it in the word list  */
-
-          fscanf (fp, "%s", wordList[wordCount]); /* Get the actual word */
-
-          printf ("%d. word: %s\n", wordCount + 1, wordList[wordCount]);
-          fseek (fp, 0,
-                 SEEK_END); /* Go to end of file for the next iteration */
-          wordCount++;
+                  int sel = rand () % wordCount + 1;
+                  for (int j = 0; j < sel; j++)
+                    {
+                      fgets (word, sizeof (word), fp);
+                    }
+                  strcpy (wordList[i], word);
+                }
+            }
         }
     }
+
+  fclose (fp);
+
+  /* Display results */
+  for (int i = 0; i < NUM_WORDS; i++)
+    {
+      printf ("%d. word: %s", i + 1, wordList[i]);
+    }
+
+  // if (fp != NULL)
+  //   {
+  //     srand (time (NULL)); // seed
+  //
+  //     /* Select random 7 words for the board */
+  //     while (wordCount < 7)
+  //       {
+  //         /* Move the position of file pointer back to a random position
+  //         */ fseek (fp, -1 * (rand () % size_file), SEEK_CUR);
+  //
+  //         /* In case pointer is not at the start of a word, scan until
+  //         end of the line and put the scanned word in temp to discard
+  //         it, then read the next word from the start andput it in the
+  //         word list  */ fscanf (fp, "%s", temp[wordCount]);
+  //
+  //         /* Get the actual word */
+  //         fscanf (fp, "%s", wordList[wordCount]);
+  //
+  //         printf ("%d. word: %s\n", wordCount + 1, wordList[wordCount]);
+  //
+  //         /* Go to end of file for the next iteration */
+  //         fseek (fp, 0, SEEK_END);
+  //         wordCount++;
+  //       }
+  //   }
   /* Build the initial board */
   for (int i = 0; i < 7; i++)
     {
@@ -357,4 +402,6 @@ main (void)
         }
       printf ("\n");
     }
+
+  return 0;
 }
