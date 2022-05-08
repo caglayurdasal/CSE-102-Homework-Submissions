d757f8f2262ee48b4162d49cdfa184f5e1407bf3
Çağla <caglayurdasal@gmail.com>
2022-05-08 13:20:46 +0300

    Add check_loop_iterative()

--- hw8/util.h
+++ hw8/util.h
@@ -2,5 +2,5 @@
 #define _UTIL_H_
 
 void generate_sequence(int xs, int currentlen, int seqlen, int *seq);
-void check_loop_iterative(void (*f)(?), int xs, int seqlen, int *loop, int *looplen);
+void check_loop_iterative(void (*f)(int, int, int, int *), int xs, int seqlen, int *loop, int *looplen);
 #endif /* _UTIL_H_ */
