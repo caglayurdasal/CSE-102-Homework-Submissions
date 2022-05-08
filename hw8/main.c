8f6de5005c415b00364c5812e1229a5ca61ca7b0
Çağla <caglayurdasal@gmail.com>
2022-05-08 19:25:55 +0300

    Reinitialize int looplen as int *looplen

--- hw8/main.c
+++ hw8/main.c
@@ -4,21 +4,21 @@
 
 int main()
 {
-	int xs, looplen;
+	int xs, *looplen;
 	static int seqlen;
 	printf("Please enter the sequence length: ");
 	scanf("%d", &seqlen);
-	looplen = seqlen / 2;
+	int seq_len = seqlen;
+	looplen = &seq_len;
+	*looplen = seq_len / 2;
 
 	printf("Please enter the first element: ");
 	scanf("%d", &xs);
 	int *loop = (int *)malloc((seqlen) * sizeof(int));
 	int myArr[9] = {3, 55, 6, 23, 2, 4, 23, 2, 4};
-	// has_loop(arr, 9, 3, ls, le);
+
 	/* Generate the sequence and check for the loop count */
-	check_loop_iterative(generate_sequence, xs, seqlen, loop, looplen);
-	// printf("Looplen=%d\n", looplen);
-	// printf("Seqlen=%d\n", seqlen);
+	check_loop_iterative(generate_sequence, xs, seqlen, loop, &seq_len);
 
 	free(loop);
 	return 0;
