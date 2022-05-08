5cc1725da304000f02306938254ae8efe423e028
Çağla <caglayurdasal@gmail.com>
2022-05-08 19:27:28 +0300

    Change function parameter looplen to *looplen

--- hw8/util.h
+++ hw8/util.h
@@ -3,5 +3,5 @@
 
 void generate_sequence(int xs, int currentlen, int seqlen, int *seq);
 int has_loop(int *arr, int n, int looplen, int *ls, int *le);
-void check_loop_iterative(void (*f)(int, int, int, int *), int xs, int seqlen, int *loop, int looplen);
+void check_loop_iterative(void (*f)(int, int, int, int *), int xs, int seqlen, int *loop, int *looplen);
 #endif /* _UTIL_H_ */
