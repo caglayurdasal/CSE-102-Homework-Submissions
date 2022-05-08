f3ed48ce1413abf8646ff85f9cc26616322a4fc7
Çağla <caglayurdasal@gmail.com>
2022-05-08 17:53:39 +0300

    Reinitialize seqlen to be static int and looplen.

--- hw8/main.c
+++ hw8/main.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "util.h"
-#define MAX 100
+
 int main()
 {
-	int seqlen, xs, loop[MAX];
+	int xs, looplen;
+	static int seqlen;
 	printf("Please enter the sequence length: ");
 	scanf("%d", &seqlen);
+	looplen = seqlen / 2;
 
 	printf("Please enter the first element: ");
 	scanf("%d", &xs);
-
+	int *loop = (int *)malloc((seqlen) * sizeof(int));
+	int myArr[9] = {3, 55, 6, 23, 2, 4, 23, 2, 4};
+	// has_loop(arr, 9, 3, ls, le);
 	/* Generate the sequence and check for the loop count */
-	check_loop_iterative(generate_sequence, xs, seqlen, loop, 0);
+	check_loop_iterative(generate_sequence, xs, seqlen, loop, looplen);
+	// printf("Looplen=%d\n", looplen);
+	// printf("Seqlen=%d\n", seqlen);
 
+	free(loop);
 	return 0;
 }
