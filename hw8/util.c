25fe598e4f3fabc3ec567e43e05c4d3648e21b6b
Çağla <caglayurdasal@gmail.com>
2022-05-08 13:19:02 +0300

    Edit base case of check_loop_iterative()

--- hw8/util.c
+++ hw8/util.c
@@ -7,7 +7,7 @@ void generate_sequence(int xs, int currentlen, int seqlen, int *seq)
     // if x%2!=0 -> 3x+1
     // else -> x/2
 
-    if (currentlen + 1 == seqlen)
+    if (seqlen == 1)
     {
         *(seq + 1) = *seq / 2; // Last item will be 1
     }
@@ -21,6 +21,36 @@ void generate_sequence(int xs, int currentlen, int seqlen, int *seq)
         {
             *(seq + 1) = xs / 2;
         }
-        generate_sequence(*(seq + 1), currentlen + 1, seqlen, seq + 1);
+        currentlen++;
+        seqlen--;
+        generate_sequence(*(seq + 1), currentlen, seqlen, seq + 1);
+    }
+}
+void check_loop_iterative(void (*f)(int, int, int, int *), int xs, int seqlen, int *loop, int *looplen)
+{
+    /* Display the sequence first */
+    if (seqlen > 0)
+    {
+        int *seq = (int *)malloc(seqlen * sizeof(int)); // Allocating enough memory for the sequence
+        seq[0] = xs;                                    // First member of sequence is given by user
+        /* Generate the sequence */
+        f(xs, 0, seqlen, seq);
+        /* Display the sequence */
+        printf("Sequence: {");
+        for (int i = 0; i < seqlen; i++)
+        {
+            if (i == seqlen - 1)
+            {
+                printf("%d}\n", seq[i]);
+            }
+            else
+            {
+                printf("%d, ", seq[i]);
+            }
+        }
+    }
+    /* After displaying the sequence, check for loop count */
+    else
+    {
     }
 }
