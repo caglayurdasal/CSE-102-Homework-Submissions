1a078893dce7d70933393b1ce96423e1ea87784b
Çağla <caglayurdasal@gmail.com>
2022-05-08 13:20:17 +0300

    Call only check_loop_iterative in main

--- hw8/main.c
+++ hw8/main.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "util.h"
-
+#define MAX 100
 int main()
 {
-	int seqlen, xs;
+	int seqlen, xs, loop[MAX];
 	printf("Please enter the sequence length: ");
 	scanf("%d", &seqlen);
-	int *seq = (int *)malloc(seqlen * sizeof(int)); // Allocating enough memory for the sequence
 
 	printf("Please enter the first element: ");
 	scanf("%d", &xs);
-	seq[0] = xs;
 
-	/* Generating the sequence */
-	generate_sequence(xs, 1, seqlen, seq);
-
-	/* Displaying the sequence */
-	printf("Sequence: {");
-	for (int i = 0; i < seqlen; i++)
-	{
-		if (i == seqlen - 1)
-		{
-			printf("%d}\n", seq[i]);
-		}
-		else
-		{
-			printf("%d, ", seq[i]);
-		}
-	}
+	/* Generate the sequence and check for the loop count */
+	check_loop_iterative(generate_sequence, xs, seqlen, loop, 0);
 
 	return 0;
 }
