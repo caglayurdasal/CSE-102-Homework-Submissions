b9147fd49b92bdbb728efc4bd25e87691acf074b
Çağla <caglayurdasal@gmail.com>
2022-05-08 22:41:38 +0300

    Add histogram

--- hw8/util.h
+++ hw8/util.h
@@ -4,5 +4,7 @@
 void generate_sequence(int xs, int currentlen, int seqlen, int *seq);
 int has_loop(int *arr, int n, int looplen, int *ls, int *le);
 void check_loop_iterative(void (*f)(int, int, int, int *), int xs, int seqlen, int *loop, int *looplen);
-void hist_of_firstdigits(void (*f)(?), int xs, int seqlen, int *h, int digit);
+void hist_of_firstdigits(void (*f)(int, int, int, int *), int xs, int seqlen, int *h, int digit);
+int firstDigit(int num);
+
 #endif /* _UTIL_H_ */
