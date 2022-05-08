1ef7c9387a5f0bc2c2edaebcb6d6eaefca37de05
Çağla <caglayurdasal@gmail.com>
2022-05-08 12:02:42 +0300

    Add generate_sequence function

---
+++ hw8/util.h
@@ -0,0 +1,6 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+void generate_sequence(int xs, int currentlen, int seqlen, int *seq);
+void check_loop_iterative(void (*f)(?), int xs, int seqlen, int *loop, int *looplen);
+#endif /* _UTIL_H_ */
