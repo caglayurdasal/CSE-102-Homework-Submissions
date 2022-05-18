56c618ec972c4c8a1780c81624355b87d542d682
caglayurdasal <caglayurdasal@gmail.com>
2022-05-18 19:14:24 +0300

    Add main menu

--- hw9/main.c
+++ hw9/main.c
@@ -23,6 +23,43 @@ struct BankAccount
 };
 int main()
 {
-    printf("Hello, World!\n");
+    FILE *fp;
+    fp = fopen("customers.txt", "a+");
+
+    int sel;
+    printf("=====================================\n");
+    printf("Welcome to the Bank Management System\n");
+    printf("=====================================\n");
+    do
+    {
+        printf("\t 1. List all customers\n");
+        printf("\t 2. Add new customer\n");
+        printf("\t 3. New Loan Application\n");
+        printf("\t 4. Report menu\n");
+        printf("\t 5. Exit System\n");
+        scanf("%d", &sel);
+        switch (sel)
+        {
+        case 1:
+            break;
+        case 2:
+            /* Add new customer information from the user */
+            printf("Customer Name: ");
+            scanf("%s", );
+            printf("Customer Address: ");
+            printf("Customer Phone: ");
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            printf("Invalid entry!\n");
+            break;
+        }
+    } while (sel != 5);
+    fclose(fp);
     return 0;
 }
