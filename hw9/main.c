0bce6c55de6057cc1986a4c9f8c8d05754b6eac7
caglayurdasal <caglayurdasal@gmail.com>
2022-05-21 13:01:17 +0300

    Edit case 1

--- hw9/main.c
+++ hw9/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+
 int listCustomers();
+
 int addCustomer();
+
 int newLoan();
-int calculateLoan(float amount, int period, float interestRate);
+
+float calculateLoan(float amount, int period, float interestRate);
+
 int getReport();
+
 union Person
 {
     char name[50];
@@ -18,15 +24,22 @@ union Loan
 };
 struct BankAccount
 {
-    union Person Customer;
+    union Person Customer[3];
     union Loan Loans[3];
 };
+
 int main()
 {
     FILE *fp;
+    struct BankAccount account;
     fp = fopen("customers.txt", "a+");
 
-    int sel;
+    int sel, num_lines = 0;
+    float amount = 10000;
+    int period = 3;
+    float interest_rate = 0.05;
+    float loan = calculateLoan(amount, period, interest_rate);
+    printf("Calculated loan: %f\n", loan);
     printf("=====================================\n");
     printf("Welcome to the Bank Management System\n");
     printf("=====================================\n");
@@ -41,13 +54,36 @@ int main()
         switch (sel)
         {
         case 1:
+            rewind(fp);
+            while (!feof(fp))
+            {
+                fscanf(fp, "%s ", account.Customer->name);
+                printf("Customer ID = %d\n", num_lines + 1);
+                printf("Customer Name = %s\n", account.Customer->name);
+                // fscanf(fp, "%d %[^\n]s", &account.Customer->phone, account.Customer->address);
+                fscanf(fp, "%d ", &account.Customer->phone);
+                printf("Customer Phone = %d\n", account.Customer->phone);
+                fscanf(fp, "%[^\n]s", account.Customer->address);
+                printf("Customer Address = %s\n", account.Customer->address);
+                num_lines++;
+            }
             break;
         case 2:
             /* Add new customer information from the user */
+            // Save the customer information in the database
             printf("Customer Name: ");
-            scanf("%s", );
-            printf("Customer Address: ");
+            fflush(stdin);
+            scanf("%[^\n]s", account.Customer->name);
+            fprintf(fp, "%s ", account.Customer->name);
             printf("Customer Phone: ");
+            fflush(stdin);
+            scanf("%d", &account.Customer->phone);
+            fprintf(fp, "%d ", account.Customer->phone);
+            printf("Customer Address: ");
+            fflush(stdin);
+            scanf("%[^\n]s", account.Customer->address);
+            fprintf(fp, "%s\n", account.Customer->address);
+
             break;
         case 3:
             break;
@@ -62,4 +98,24 @@ int main()
     } while (sel != 5);
     fclose(fp);
     return 0;
+}
+
+float calculateLoan(float amount, int period, float interestRate)
+{
+    /* FV = PV (1 + i)^n
+     * For example, the future value of $10,000 compounded at 5% annually for three years:
+     *      = $10,000 (1 + 0.05)^3
+     *      = $10,000 (1.157625)
+     *      = $11,576.25 */
+    float loan = amount;
+
+    if (period == 1)
+    {
+        loan = amount * (1 + interestRate) * period;
+        return loan;
+    }
+    else
+    {
+        return ((1 + interestRate) * calculateLoan(amount, period - 1, interestRate));
+    }
 }
