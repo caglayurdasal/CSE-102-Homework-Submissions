bd04570fcc22378750c6d0f072d4e9da154c13b6
caglayurdasal <caglayurdasal@gmail.com>
2022-05-23 03:37:58 +0300

    Add comments

--- hw9/main.c
+++ hw9/main.c
@@ -1,3 +1,6 @@
+/*
+ * Assignment #9 submission by Çağla Nur Yurdasal
+ */
 #include <stdio.h>
 
 void listCustomers();
@@ -26,14 +29,15 @@ struct BankAccount
     union Person Customer;
     union Loan Loans[3];
 };
-struct BankAccount account[50];
-static int num_customers = 0;
-int loan_check[50][3] = {0};
-int periods[50][3];
+struct BankAccount account[50]; // struct to keep customer and loan information
+static int num_customers = 0;   // number of customers
+int loan_check[50][3] = {0};    // check number of loans
+int periods[50][3];             // check periods
 
 int main()
 {
-    int sel, loan_check;
+    int sel,        // user selection
+        loan_check; // variable to check if selected customer can apply for a new loan
 
     printf("=====================================\n");
     printf("Welcome to the Bank Management System\n");
@@ -49,27 +53,32 @@ int main()
         switch (sel)
         {
         case 1:
+            /* List all customers saved in the database */
             listCustomers();
             break;
         case 2:
+            /* Add a new customer to be saved in the database */
             addCustomer();
             break;
         case 3:
             loan_check = newLoan();
             if (loan_check == 0)
             {
+                /* Inform the user if a new loan can't be given to chosen customer */
                 printf("No more loans can be given to this customer.\n");
             }
             else if (loan_check == -1)
             {
+                /* Inform the user if an ID that is not saved in the database is entered */
                 printf("You have entered an invalid ID\n");
             }
-
             break;
         case 4:
+            /* Give the report on customer or credit details */
             getReport();
             break;
         case 5:
+            /* Stop the program if user enters 5 to exit */
             break;
         default:
             printf("Invalid entry!\n");
@@ -82,8 +91,8 @@ int main()
 void addCustomer()
 {
     /* Add new customer information from the user and
-     * save the customer information in the database*/
-    char buffer[100];
+     * save it in the database
+     */
     FILE *fp;
     fp = fopen("customers.txt", "a+");
 
@@ -95,10 +104,6 @@ void addCustomer()
     fflush(stdin);
     scanf("%[^\n]s", account[num_customers].Customer.address);
     fprintf(fp, "%s ", account[num_customers].Customer.address);
-    // printf("Customer ID: ");
-    // fflush(stdin);
-    // scanf("%d", &account[num_customers].Customer.id);
-    // fprintf(fp, "%d ", account[num_customers].Customer.id);
     printf("Customer Name: ");
     fflush(stdin);
     scanf("%[^\n]s", account[num_customers].Customer.name);
@@ -109,6 +114,7 @@ void addCustomer()
 }
 void listCustomers()
 {
+    /* List customer ID and names */
     for (int i = 0; i < num_customers; i++)
     {
         printf("Customer ID = %d\n", i + 1);
@@ -117,20 +123,22 @@ void listCustomers()
 }
 float calculateLoan(float amount, int period, float interestRate)
 {
-    /* FV = PV (1 + i)^n
+    /* Calculate the loan using the formula FV = PV (1 + i)^n
      * For example, the future value of $10,000 compounded at 5% annually for three years:
      *      = $10,000 (1 + 0.05)^3
      *      = $10,000 (1.157625)
      *      = $11,576.25 */
     float loan = amount;
 
-    if (period == 1)
+    if (period == 1) // base case
     {
+        /* if period==1, multiply it by the rest of the formula as n^1==n*1 */
         loan = amount * (1 + interestRate) * period;
         return loan;
     }
     else
     {
+        /* Keep calling the function to multiply the (1+interestRate) with itself period times */
         return ((1 + interestRate) * calculateLoan(amount, period - 1, interestRate));
     }
 }
@@ -138,12 +146,17 @@ void getReport()
 {
     FILE *fp;
     fp = fopen("customers.txt", "r");
-    int sel, which_credit, which_customer, i = 0;
+    int sel,            // user selection
+        which_credit,   // credit of which details to be displayed
+        which_customer, // customer whom details to be displayed
+        i = 0;          // variable to keep track of saved customers in the database
     printf("Enter 1 for customer details, 2 for credit details: ");
     scanf("%d", &sel);
     if (sel == 1)
     {
         rewind(fp);
+        /* Display all the saved customers' details saved in the customers.txt
+         * database using the struct account */
         while (i < num_customers)
         {
             printf("Customer ID = %d\n", i + 1);
@@ -162,7 +175,9 @@ void getReport()
         scanf("%d", &which_customer);
         printf("Which credit do you want to display? ");
         scanf("%d", &which_credit);
+        /* Display the chosen loan details of the chosen customer */
         printf("Total credit value = %.4f\n", account[which_customer - 1].Loans->arr[which_credit - 1]);
+        /* Display all the installements of the chosen loan */
         for (int m = 0; m < periods[which_customer - 1][which_credit - 1]; m++)
         {
             printf("%d. month installment = %.4f\n", m + 1, account[which_customer - 1].Loans->arr[which_credit - 1] / (float)periods[which_customer - 1][which_credit - 1]);
@@ -171,8 +186,14 @@ void getReport()
 }
 int newLoan()
 {
-    int customer_id, loan_amount = 0, current_id = 1, id_check = 0;
-    float loan, auxAmount, auxPeriod, auxInterestRate;
+    int customer_id,     // customer to apply for a new loan
+        loan_amount = 0, // number of loans given to the customer so far
+        current_id = 1,  // variable to check if entered ID is valid
+        id_check = 0;    // variable to validate the entered ID
+    float loan,          // loan to be given
+        auxAmount,       // amount of credit
+        auxPeriod,       // number of months
+        auxInterestRate; // interest rate
     char buffer[50];
     FILE *fp;
     fp = fopen("customers.txt", "r");
@@ -180,9 +201,11 @@ int newLoan()
     printf("Which customer do you want to give the loan to? Enter ID: ");
     scanf("%d", &customer_id);
 
+    /* Check whether the entered customer ID is a valid one by searching the database */
     while (!feof(fp))
     {
         fgets(buffer, 50, fp);
+        /* If entered ID is found in the database, ID is valid */
         if (current_id == customer_id)
         {
             id_check++;
@@ -193,11 +216,12 @@ int newLoan()
         }
     }
     fclose(fp);
+    /* If entered ID is not saved in the database, return -1 to give an error message */
     if (id_check == 0)
     {
         return -1;
     }
-
+    /* Check whether the chosen customer can apply for a new loan */
     for (int i = 0; i < 3; i++)
     {
         if (loan_check[customer_id - 1][i] > 0)
@@ -216,6 +240,7 @@ int newLoan()
             printf("Enter period: ");
             fflush(stdin);
             scanf("%f", &auxPeriod);
+            /* Save the periods in another database */
             periods[customer_id - 1][i] = auxPeriod;
             printf("Enter interest rate: ");
             fflush(stdin);
@@ -224,6 +249,7 @@ int newLoan()
             /* Calculate the loan */
             loan = calculateLoan(auxAmount, auxPeriod, auxInterestRate);
             loan_check[customer_id - 1][i] = loan;
+            /* Save the loan to customer loan union */
             account[customer_id - 1].Loans->arr[i] = loan;
 
             return 1;
